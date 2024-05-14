9a2b58583a03d7f9f7c81e17888c15ca1a955a49
ankitkrissna1234 <ankitkusahu7846@gmail.com>
2024-05-14 17:18:16 +0530

    exception and logger code added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'acneDetection',
+    version= '0.0.0',
+    author= 'ankit',
+    author_email= 'ankitkusahu7846@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
